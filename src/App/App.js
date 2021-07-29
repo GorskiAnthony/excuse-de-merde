22a4a97329508efae1148d7a0bbeddff2aa4d023
AnthonyGorski <tony.gorski13@gmail.com>
2021-07-29 16:55:18 +0200

    add admin

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
 import Home from "./pages/Home";
-import Admin from "./pages/Admin";
 import Dashboard from "./pages/Dashboard";
 import Add from "./pages/Add";
 import "../assets/index.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  const [login, setLogin] = useState(false);
   const [isActive, setActive] = useState(false);
-
-  const handleLogin = (data) => {
-    setLogin(data);
-  };
 
   return (
     <Router>
@@ -31,20 +25,9 @@ const App = () => {
             <Add {...props} isActive={isActive} setActive={setActive} />
           )}
         />
-        {login ? (
-          <Route
-            path="/admin/dashboard"
-            component={(props) => <Dashboard {...props} login={login} />}
-          />
-        ) : (
-          <Route
-            path="/admin"
-            component={(props) => <Admin {...props} handleLog={handleLogin} />}
-          />
-        )}
         <Route
-          path="/admin"
-          component={(props) => <Admin {...props} handleLog={handleLogin} />}
+          path="/dashboard"
+          component={(props) => <Dashboard {...props} />}
         />
       </Switch>
     </Router>
